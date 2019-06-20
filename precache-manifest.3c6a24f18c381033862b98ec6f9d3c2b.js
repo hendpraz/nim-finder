9f19f93e969b81b9de870b942402326bba81d655
Muhammad Hendry Prasetya <45161697+hendpraz@users.noreply.github.com>
2019-06-20 20:15:32 +0700

    Updates

---
+++ precache-manifest.3c6a24f18c381033862b98ec6f9d3c2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6829551139d5b10c2888448f6905ab82",
+    "url": "/nim-finder/index.html"
+  },
+  {
+    "revision": "68d7c1099c732db03ee4",
+    "url": "/nim-finder/static/css/main.9f7d6c43.chunk.css"
+  },
+  {
+    "revision": "777f221a75d5d5d0f1c0",
+    "url": "/nim-finder/static/js/2.44c34550.chunk.js"
+  },
+  {
+    "revision": "68d7c1099c732db03ee4",
+    "url": "/nim-finder/static/js/main.4e765998.chunk.js"
+  },
+  {
+    "revision": "bcf8370f0404171e2f85",
+    "url": "/nim-finder/static/js/runtime~main.5854a2c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nim-finder/static/media/logo.5d5d9eef.svg"
+  }
+]);
